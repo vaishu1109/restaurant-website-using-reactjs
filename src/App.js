c3a78f0b4c48d38610121cddbe2fcef2c4f9873d
Vaishnavi <vaishu.sg.1109@gmail.com>
2024-08-22 20:54:41 +0530

    Home page design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import "./assets/css/style.css";
-
+import Home from './components/Home/Home';
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home />
     </div>
   )
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../Home/Home.css"
+const Home = () => {
+    return (
+        <>
+            <section className='home' id='home'>
+                <div className='content'>
+                    <h3>fresh <span>food in the</span> morning</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repudiandae vel nobis nam quis blanditiis accusamus iusto delectus sint ipsum.
+                    </p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Home
