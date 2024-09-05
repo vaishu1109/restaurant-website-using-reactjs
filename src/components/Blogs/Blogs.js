01932fa6ca577eca5792160222ae6e64c556fc13
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-05 22:55:11 +0530

    Design complete of blog page

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../Blogs/Blogs.css"
+import { blog } from "../../Data";
 const Blogs = () => {
   return (
     <>
@@ -7,6 +8,27 @@ const Blogs = () => {
             <h1 className='heading'>
                 Our <span>blogs</span>
             </h1>
+            <div className='box-container'>
+              {blog.map((item,index) => (
+                <div className='box' key={index}>
+                  <div className='image'>
+                    <img src={item.img} alt='blogpic'/>
+                  </div>
+                  <div className='content'>
+                    <a href='#' className='title'>
+                      Tasty and refresh spices
+                    </a>
+                    <span>by admin / 21st may, 2024</span>
+                    <p>
+                      random text
+                    </p>
+                    <a href='#' className='btn'>
+                      read more
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
         </section>
     </>
   )
